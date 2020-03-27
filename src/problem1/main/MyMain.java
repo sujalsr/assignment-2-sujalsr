5172d0224c15abcc0ef2953158d3eadb8bfaea6d
Sujal Singh <55826559+sujalsr@users.noreply.github.com>
2020-03-27 21:41:04 +0530

    problem 1 BST Algorithm for left nodes

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,21 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(10);
+        obj.insert(2);
+        obj.insert(3);
+        obj.insert(5);
+        obj.insert(4);
+        obj.insert(8);
+        obj.insert(1);
+        System.out.println("Left Children (Nodes)");
+        obj.printLeftNode();
+        System.out.println("Count of Nodes not having left child");
+        obj.countNotLeft();
+    }
 }
