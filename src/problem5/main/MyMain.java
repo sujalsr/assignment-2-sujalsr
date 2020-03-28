fa51002a27b628d50cc357c96e7f02874883b419
Sujal Singh <55826559+sujalsr@users.noreply.github.com>
2020-03-28 17:05:29 +0530

    final commit

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -5,7 +5,21 @@
  *  Time: 7:06 PM
  */
 package problem5.main;
+
+import problem5.circularqueue.MyCircularQueue;
+
 //executable class
 public class MyMain {
+    public static void main(String[] args) {
+        MyCircularQueue obj = new MyCircularQueue();
+        obj.enqueue(1, 4, 2);
+        obj.enqueue(2, 4, 1);
+        obj.enqueue(48, 0, 3);
+        obj.enqueue(5, 2, 0);
+        obj.enqueue(39, 2, 2);
+        obj.enqueue(69, 0, 1);
+        obj.eraseBackLog();
+        obj.display();
 
+    }
 }
