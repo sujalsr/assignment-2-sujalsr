3868291d47dd2cc35407da244e5be8465f782ba8
Sujal Singh <55826559+sujalsr@users.noreply.github.com>
2020-03-28 16:44:22 +0530

    problem 5 student and node class commit

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -7,4 +7,40 @@
 package problem5.student;
 // to store student information and properties
 public class Student {
+    int roll;
+    int app_count;
+    int backlog;
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public int getApp_count() {
+        return app_count;
+    }
+
+    public void setApp_count(int app_count) {
+        this.app_count = app_count;
+    }
+
+    public int getBacklog() {
+        return backlog;
+    }
+
+    public void setBacklog(int backlog) {
+        this.backlog = backlog;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "roll=" + roll +
+                ", app_count=" + app_count +
+                ", backlog=" + backlog +
+                '}';
+    }
 }
